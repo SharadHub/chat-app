f824143588c13824987f57391d5790474c2d8357
Sharad Bista <bishalbista737@gmail.com>
2026-04-04 23:57:26 +0545

    refactor(db): improve database connection setup

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-
+import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect('mongodb://localhost:27017/chat-app-db');
